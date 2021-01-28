a427dfd7fc1c1cead5c88dbadf3ebe6eee83d8b3
David Gilbert <dave@jfree.org>
2021-01-28 06:03:04 +0100

    Dataset cleanup.

--- src/main/java/org/jfree/data/flow/DefaultFlowDataset.java
+++ src/main/java/org/jfree/data/flow/DefaultFlowDataset.java
@@ -43,12 +43,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import org.jfree.chart.plot.flow.FlowUtils;
+import org.jfree.chart.util.Args;
 import org.jfree.chart.util.PublicCloneable;
 import org.jfree.data.general.AbstractDataset;
 
 /**
  * A dataset representing flows between source and destination nodes.
- * @param <K>
+ * 
+ * @param <K> the type for the keys used to identify sources and destinations 
+ *     (instances should be immutable, {@code String} is a good default choice).
  */
 public class DefaultFlowDataset<K extends Comparable<K>> extends AbstractDataset 
         implements FlowDataset<K>, PublicCloneable, Serializable {
@@ -59,29 +62,70 @@ public class DefaultFlowDataset<K extends Comparable<K>> extends AbstractDataset
     /** Storage for the flows. */
     private Map<FlowKey<K>, Number> flows;
     
+    /**
+     * Creates a new dataset that is initially empty.
+     */
     public DefaultFlowDataset() {
         this.nodes = new ArrayList<>();
         this.nodes.add(new ArrayList<>());
         this.nodes.add(new ArrayList<>());
         this.flows = new HashMap<>();
     }
-    
+
+    /**
+     * Returns a list of the source nodes for the specified stage.
+     * 
+     * @param stage  the stage (0 to {@code getStageCount() - 1}).
+     * 
+     * @return A list of source nodes (possibly empty but never {@code null}). 
+     */
     @Override
     public List<K> getSources(int stage) {
         return new ArrayList<>(this.nodes.get(stage));
     }
 
+    /**
+     * Returns a list of the destination nodes for the specified stage.
+     * 
+     * @param stage  the stage (0 to {@code getStageCount() - 1}).
+     * 
+     * @return A list of destination nodes (possibly empty but never {@code null}). 
+     */
     @Override
     public List<K> getDestinations(int stage) {
         return new ArrayList<>(this.nodes.get(stage + 1));
     }
 
+    /**
+     * Returns the flow between a source node and a destination node at a
+     * specified stage.  This must be 0 or greater.  The dataset can return
+     * {@code null} to represent an unknown value.
+     * 
+     * @param stage  the stage index (0 to {@code getStageCount()} - 1).
+     * @param source  the source ({@code null} not permitted). 
+     * @param destination  the destination ({@code null} not permitted).
+     * 
+     * @return The flow (zero or greater, possibly {@code null}). 
+     */
     @Override
     public Number getFlow(int stage, K source, K destination) {
         return this.flows.get(new FlowKey<>(stage, source, destination));
     }
-    
+
+    /**
+     * Sets the flow between a source node and a destination node at the 
+     * specified stage.  A new stage will be added if {@code stage} is equal
+     * to {@code getStageCount()}.
+     * 
+     * @param stage  the stage (0 to {@code getStageCount()}.
+     * @param source  the source ({@code null} not permitted).
+     * @param destination  the destination ({@code null} not permitted).
+     * @param flow  the flow (0 or greater).
+     */
     public void setFlow(int stage, K source, K destination, double flow) {
+        Args.requireInRange(stage, "stage", 0, getStageCount());
+        Args.nullNotPermitted(source, "source");
+        Args.nullNotPermitted(destination, "destination");
         if (stage > this.nodes.size() - 2) {
             this.nodes.add(new ArrayList<>());
         }
@@ -92,13 +136,28 @@ public class DefaultFlowDataset<K extends Comparable<K>> extends AbstractDataset
             this.nodes.get(stage + 1).add(destination);
         }
         this.flows.put(new FlowKey<>(stage, source, destination), flow);
+        fireDatasetChanged();
     }
 
+    /**
+     * Returns the number of flow stages.  A flow dataset always has one or
+     * more stages, so this method will return {@code 1} even for an empty
+     * dataset (one with no sources, destinations or flows defined).
+     * 
+     * @return The number of flow stages.
+     */
     @Override
     public int getStageCount() {
         return this.nodes.size() - 1;
     }
 
+    /**
+     * Returns a clone of the dataset.
+     * 
+     * @return A clone of the dataset.
+     * 
+     * @throws CloneNotSupportedException 
+     */
     @Override
     public Object clone() throws CloneNotSupportedException {
         DefaultFlowDataset<K> clone = (DefaultFlowDataset) super.clone();
@@ -110,23 +169,42 @@ public class DefaultFlowDataset<K extends Comparable<K>> extends AbstractDataset
         return clone;
     }
 
+    /**
+     * Tests this dataset for equality with an arbitrary object.  This method
+     * will return {@code true} if the object implements the 
+     * {@link FlowDataset} and defines the exact same set of nodes and flows 
+     * as this dataset.
+     * 
+     * @param obj  the object to test equality against ({@code null} permitted).
+     * 
+     * @return A boolean. 
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-        if (obj == null) {
+        if (!(obj instanceof FlowDataset)) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        final FlowDataset other = (FlowDataset) obj;
+        if (other.getStageCount() != getStageCount()) {
             return false;
         }
-        final DefaultFlowDataset<?> other = (DefaultFlowDataset<?>) obj;
-        if (!Objects.equals(this.nodes, other.nodes)) {
-            return false;
-        }
-        if (!Objects.equals(this.flows, other.flows)) {
-            return false;
+        for (int stage = 0; stage < getStageCount(); stage++) {
+            if (!Objects.equals(other.getSources(stage), getSources(stage))) {
+                return false;
+            }
+            if (!Objects.equals(other.getDestinations(stage), getDestinations(stage))) {
+                return false;
+            }
+            for (K source : getSources(stage)) {
+                for (K destination : getDestinations(stage)) {
+                    if (!Objects.equals(other.getFlow(stage, source, destination), getFlow(stage, source, destination))) {
+                        return false;
+                    }
+                }
+            }
         }
         return true;
     }
@@ -134,10 +212,9 @@ public class DefaultFlowDataset<K extends Comparable<K>> extends AbstractDataset
     @Override
     public int hashCode() {
         int hash = 3;
-        hash = 89 * hash + Objects.hashCode(this.nodes);
-        hash = 89 * hash + Objects.hashCode(this.flows);
+        hash = 89 * hash + Objects.hashCode(getSources(0));
+        hash = 89 * hash + Objects.hashCode(getDestinations(getStageCount() - 1));
         return hash;
     }
-    
-    
+
 }
--- src/main/java/org/jfree/data/flow/FlowDataset.java
+++ src/main/java/org/jfree/data/flow/FlowDataset.java
@@ -40,15 +40,23 @@ import java.util.List;
 import org.jfree.data.general.Dataset;
 
 /**
- * A dataset representing flows from a set of sources to a set of destinations.
+ * A dataset representing value flows from a set of source nodes to a set of 
+ * destination nodes.  The number of source nodes does not need to match the
+ * number of destination nodes.
+ * <br><br>
+ * The dataset can represent flows in one or many stages.  In the simple case,
+ * the flows are defined in one stage, going directly from a source node to 
+ * the final destination node.  In a multi-stage dataset there will be groups
+ * of intermediate nodes and the flows are defined stage-by-stage from the
+ * source to the destination.
  * 
  * @param <K> the type for the keys used to identify sources and destinations 
- *     ({@code String} is a good default choice).
+ *     (instances should be immutable, {@code String} is a good default choice).
  */
 public interface FlowDataset<K extends Comparable<K>> extends Dataset {
     
     /**
-     * Returns the number of flow stages.  
+     * Returns the number of flow stages (never less than one).
      * 
      * @return The number of flow stages.
      */
@@ -57,29 +65,33 @@ public interface FlowDataset<K extends Comparable<K>> extends Dataset {
     /**
      * Returns a list of the sources at the specified stage.
      * 
-     * @param stage  the stage index.
+     * @param stage  the stage index (0 to {@code getStageCount()} - 1).
      * 
-     * @return A list of the sources at the specified stage.
+     * @return A list of the sources at the specified stage (never {@code null}).
      */
     List<K> getSources(int stage);
     
     /**
-     * Returns a list of the destinations at the specified stage.
+     * Returns a list of the destinations at the specified stage.  For a 
+     * multi-stage dataset, the destinations at stage N are the same as the
+     * sources at stage N+1.
      * 
-     * @param stage  the stage index.
+     * @param stage  the stage index (0 to {@code getStageCount()} - 1).
      * 
-     * @return 
+     * @return A list of the sources at the specified stage (never {@code null}).
      */
     List<K> getDestinations(int stage);
     
     /**
      * Returns the flow between a source node and a destination node at a
-     * specified stage.
+     * specified stage.  This must be 0 or greater.  The dataset can return
+     * {@code null} to represent an unknown value.
      * 
-     * @param stage
-     * @param source
-     * @param destination
-     * @return 
+     * @param stage  the stage index (0 to {@code getStageCount()} - 1).
+     * @param source  the source ({@code null} not permitted). 
+     * @param destination  the destination ({@code null} not permitted).
+     * 
+     * @return The flow (zero or greater, possibly {@code null}). 
      */
     Number getFlow(int stage, K source, K destination);
     
