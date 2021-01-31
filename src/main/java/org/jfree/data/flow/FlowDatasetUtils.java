2d431a08a93b926d242091e6285c6229c027f1b6
David Gilbert <dave@jfree.org>
2021-01-31 16:58:36 +0100

    Add Javadocs.

--- src/main/java/org/jfree/data/flow/FlowDatasetUtils.java
+++ src/main/java/org/jfree/data/flow/FlowDatasetUtils.java
@@ -1,10 +1,61 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2021, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ---------------------
+ * FlowDatasetUtils.java
+ * ---------------------
+ * (C) Copyright 2021, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.data.flow;
 
 import java.util.List;
+import org.jfree.graphics2d.Args;
 
+/**
+ * Utility methods related to {@link FlowDataset}.
+ */
 public class FlowDatasetUtils {
     
+    /**
+     * Returns the total inflow for the specified destination node.
+     * 
+     * @param <K>
+     * @param dataset  the dataset ({@code null} not permitted).
+     * @param destination  the destination node ({@code null} not permitted).
+     * @param stage  the stage.
+     * 
+     * @return The total inflow volume. 
+     */
     public static <K extends Comparable<K>> double calculateInflow(FlowDataset<K> dataset, K destination, int stage) {
+        Args.nullNotPermitted(dataset, "dataset");
         if (stage == 0) {
             return 0.0;  // there are no inflows for stage 0
         }
@@ -18,7 +69,17 @@ public class FlowDatasetUtils {
         }
         return inflow;
     }
-    
+
+    /**
+     * Returns the total outflow for the specified source node.
+     * 
+     * @param <K>
+     * @param dataset  the dataset ({@code null} not permitted).
+     * @param source  the source node ({@code null} not permitted).
+     * @param stage  the stage.
+     * 
+     * @return The total outflow volume.
+     */
     public static <K extends Comparable<K>> double calculateOutflow(FlowDataset<K> dataset, K source, int stage) {
         if (stage >= dataset.getStageCount()) {
             return 0.0;  // there are no outflows for the last stage
@@ -34,6 +95,15 @@ public class FlowDatasetUtils {
         return outflow;
     }
 
+    /**
+     * Returns the total flow from all sources to all destinations at the 
+     * specified stage.
+     * 
+     * @param dataset  the dataset ({@code null} not permitted).
+     * @param stage  the stage.
+     * 
+     * @return The total flow.
+     */
     public static double calculateTotalFlow(FlowDataset dataset, int stage) {
         double total = 0.0;
         for (Object source : dataset.getSources(stage)) {
